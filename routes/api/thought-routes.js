8cb183e89b734e88ae28349e30db1b6afd49ee64
JonWaaler <jonwaaler@outlook.com>
2021-07-07 14:23:42 -0400

    thought and reaction routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const {
+  getAllThoughts,
+  getThoughtsById,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require("../../controllers/thought-controller");
+
+router.route("/").get(getAllThoughts).post(createThought);
+router
+  .route("/:id")
+  .get(getThoughtsById)
+  .put(updateThought)
+  .delete(deleteThought);
+router.route("/:thoughtId/reactions").post(createReaction);
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
